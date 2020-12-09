0270c3ddb24ded68f9939a880552bd4830864943
Eduar <juaneduardo021299@hotmail.com>
2020-12-09 13:08:09 -0600

    agregando rutas Route::resource

--- app/Http/Controllers/NoticiasPublicidadController.php
+++ app/Http/Controllers/NoticiasPublicidadController.php
@@ -10,11 +10,11 @@ class NoticiasPublicidadController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
     public function index()
     {
-        //
+        return view('Panel.index');
     }
 
     /**
