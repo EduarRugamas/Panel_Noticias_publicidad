b210c09ed0e69601ba1487e0f7d9f952a8c16264
siit2020 <soporte_tecnico@si1t.com>
2020-12-09 10:22:56 -0600

    Creacion Modelo

---
+++ app/Http/Controllers/NoticiasPublicidadController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Noticias_Publicidad;
+use Illuminate\Http\Request;
+
+class NoticiasPublicidadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Noticias_Publicidad  $noticias_Publicidad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Noticias_Publicidad $noticias_Publicidad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Noticias_Publicidad  $noticias_Publicidad
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Noticias_Publicidad $noticias_Publicidad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Noticias_Publicidad  $noticias_Publicidad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Noticias_Publicidad $noticias_Publicidad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Noticias_Publicidad  $noticias_Publicidad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Noticias_Publicidad $noticias_Publicidad)
+    {
+        //
+    }
+}
