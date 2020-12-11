f2f41e2cfb8d1d45825bde768e8bcb6a4e5b0b35
siit2020 <soporte_tecnico@si1t.com>
2020-12-11 15:29:16 -0600

    Mostrando Noticias

--- resources/views/Panel/Noticias/index.blade.php
+++ resources/views/Panel/Noticias/index.blade.php
@@ -1,4 +1,4 @@
-{{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">--}}
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
 
 @extends('Plantilla.index_plantila')
@@ -11,22 +11,20 @@
                  {{$loop->iteration}}
             <div class="header">
                 <a href="#">
-                    <img src="images/luna.jpg" alt="" width="150" height="150">
+                <img src="{{ asset('storage'.'/'.$items->Foto)}}" alt="" width="150" height="150">
                 </a>
                 <h4>{{$items-> Titulo}}</h4>
             </div>
             <div class="description">
                 <p>
-                    Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Aperiam deserunt ea esse exercitationem
-                    incidunt magni maxime nam nisi non odio perferendis, quae quam qui quibusdam ratione
-                    recusandae sequi sit vel?
+                   {!!$items->Descripcion!!}
 
                 </p>
                 {{-- para agregar iconos para redes sociales--}}
                 {{--            <div class="social">--}}
                 {{--            </div>--}}
 
+                <p>Publicado:{{$items->Fecha}}</p>
             </div>
         </div>
         @endforeach
